40bef31343b0218359348704777bdb06fe0c3eae
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-12-12 11:47:08 -0800

    add layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,89 @@
 <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/settings">About</a>
+    <div class="nav-container-80">
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <a href="/">
+            <img src="/real-home-logo.png" alt="site logo image" width="100">
+        </a>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/settings">Settings</a></li> 
+            <li><a href="/contact">Contact</a></li> 
+        </ul>
+    </div>
 </nav>
 
-<slot></slot>
+<div class="body-container-80">
+    <slot></slot>
+</div>
 
 <footer>
-    <p>Copyright @2022</p>
+    <div class="footer-container-80">
+        <p>Copyright @2022</p>
+    </div>
 </footer>
+
+<!-- Layout styles -->
+<style>
+    :global(body) {
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    nav {
+        background-color: #f1f1f1;
+        padding: 1rem 0;
+    }
+
+    .nav-container-80 {
+        max-width: 80%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .body-container-80 {
+        max-width: 80%;
+        min-height: 100vh;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .footer-container-80 {
+        max-width: 80%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+    }
+
+    li {
+        margin-left: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: #000;
+        font-size: 1.2rem;
+    }
+
+    a:hover {
+        color: rgb(10, 142, 213);
+    }
+
+    a:active {
+        color: #000;
+    }
+
+    footer {
+        background-color: #f1f1f1;
+        padding: 1rem 0;
+    }
+</style>
